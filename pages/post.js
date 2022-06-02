94497f2912b1ed1da13752220b4c043c69d10487
Estudiantes RSEF <estudiantes@rsef.es>
2022-06-02 15:05:45 +0200

    Revert "[AF] Form can't be submitted. Added onChange={onChange} to try fix it"
    This reverts commit 1d81f918c8d1ca4c83c9520ad7c8a85c6bbf0158.

--- pages/post.js
+++ pages/post.js
@@ -66,7 +66,7 @@ const Post = () => {
         </Heading>
         <FormControl id="testeo" isRequired mb={marginBetweenElements}>
           <FormLabel>Opportunity's checkbox test</FormLabel>
-          <Checkbox onChange={onChange}>Test checkbox</Checkbox>
+          <Checkbox defaultIsChecked>Test checkbox</Checkbox>
         </FormControl>
         <FormControl id="title" isRequired mb={marginBetweenElements}>
           <FormLabel>Opportunity's title</FormLabel>
