350c6216471826be9b9a3171258da0994f95599f
Estudiantes RSEF <estudiantes@rsef.es>
2022-06-02 15:04:52 +0200

    Revert "[AF] It worked! Simple checkbox dominated!"
    This reverts commit ee421b25db41cc02b084065255519d5139a687c9.

--- pages/post.js
+++ pages/post.js
@@ -66,7 +66,7 @@ const Post = () => {
         </Heading>
         <FormControl id="premium" isRequired mb={marginBetweenElements}>
           <FormLabel>Premum Opportunity Checkbox test</FormLabel>
-          <Checkbox type="text" name="premium" value="false" onChange={onChange} variant="filled">Simple Checkbox Dominated!</Checkbox>
+          <Checkbox type="text" name="premium" value="false" onChange={onChange} variant="filled">False</Checkbox>
         </FormControl>
         <FormControl id="title" isRequired mb={marginBetweenElements}>
           <FormLabel>Opportunity's title</FormLabel>
